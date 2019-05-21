b479b13082b35cfc1e840075acd061c62a1492f7
Tristan Kalmbach <tristan.kalmbach@gmail.com>
2019-05-21 15:18:24 -0400

    Finish

--- functions/index.js
+++ functions/index.js
@@ -8,35 +8,30 @@ const mailTransport = nodemailer.createTransport({
     auth: {
         user: gmailEmail,
         pass: gmailPassword
-    },
+    }
 });
 
-const ContactData = {
-    Email: String,
-    Name: String,
-    Address: String,
-    Address2: String,
-    City: String,
-    State: String,
-    Zip: Number,
-    Message: String,
-    ContactAllowed: Boolean, // Can I contact them with future emails
-    ContactDate: Date, // Date contact originally sent me a message
-}
+exports.sendContactEmail = functions.firestore.document('contacts/{contactId}').onCreate((snap, context) => {
+    const data = snap.data();
+    const who = data.Name;
+    const email = data.Email;
+    const message = data.Message;
 
-exports.sendContactEmail = functions.https.onCall((data, context) => {
-    console.log(data);
-    ContactEmail(data);
+    if (!data || !who || !email || !message) {
+        console.log('Error executing cloud function.');
+        return;
+    }
+
+    mailTransport.sendMail(
+        {
+            from: who,
+            to: 'Tristan Kalmbach <tristan.kalmbach@gmail.com>',
+            subject: 'New Contact - ' + email,
+            text: 'Message: ' + message
+        },
+        (error, info) => {
+            console.log(error);
+            console.log(info);
+        }
+    );
 });
-
-async function ContactEmail(data) {    
-    mailTransport.sendMail({
-        from: data.Name + '<' + data.Email + '>',
-        to: 'Tristan Kalmbach <tristan.kalmbach@gmail.com>',
-        subject: 'New Contact - ' + data.Name,
-        text: 'Message: ' + data.Message,
-    }, (error, info) => {
-        console.log(error);
-        console.log(info);
-    });
-}
--- js/core.js
+++ js/core.js
@@ -11,6 +11,7 @@ var firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
 
 M.AutoInit();
 
@@ -56,30 +57,96 @@ anime
         delay: 1000
     });
 
+/* CHARTS */
+var ctx = document.getElementById('myChart');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Jan', 'Feb', 'March', 'April', 'May', 'June (projected)'],
+        datasets: [
+            {
+                label: '# of Sales',
+                data: [6, 11, 21, 38, 50, 72],
+                backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)'],
+                borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)'],
+                borderWidth: 1
+            }
+        ]
+    },
+    options: {
+        scales: {
+            yAxes: [
+                {
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }
+            ]
+        }
+    }
+});
+
+const chartTwo = document.getElementById('myOtherChart');
+const myOtherChart = new Chart(chartTwo, {
+    data: {
+        datasets: [
+            {
+                data: [425, 71, 113],
+                backgroundColor: ['rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)', 'rgba(255, 99, 132, 1)']
+            }
+        ],
+
+        // These labels appear in the legend and in the tooltips when hovering different arcs
+        labels: ['Sold', 'Unsold', 'In Process']
+    },
+    type: 'pie',
+    options: {}
+});
+
+$(document).ready(function() {
+    $('#message, #state').characterCounter();
+});
+
+/* CONTACT */
 $('#submit-contact').click(() => {
-    const email = document.getElementById('inputEmail').value;
-    const name = document.getElementById('inputName').value;
-    const address = document.getElementById('inputAddress').value;
-    const address2 = document.getElementById('inputAddress2').value;
-    const city = document.getElementById('inputCity').value;
-    const state = document.getElementById('inputState').value;
-    const zip = document.getElementById('inputZip').value;
-    const message = document.getElementById('inputMessage').value;
+    const email = document.getElementById('email').value;
+    const name = document.getElementById('name').value;
+    const addr = document.getElementById('addr').value;
+    const city = document.getElementById('city').value;
+    const state = document.getElementById('state').value;
+    const zip = document.getElementById('zip').value;
+    const message = document.getElementById('message').value;
+    const allow = document.getElementById('allow').value;
+
+    if (!email || !name || !addr || !city || !state || !zip || !message) {
+        console.log('There was an issue... one of the fields entered was invalid.');
+        M.toast({ html: 'There was an issue submitting your message.' });
+        return;
+    }
+
+    console.log('Submitting contact to firebase...');
 
     const data = {
         Email: email,
         Name: name,
-        Address: address,
-        Address2: address2,
+        Address: addr,
         City: city,
         State: state,
         Zip: zip,
         Message: message,
-        ContactAllowed: true,
+        ContactAllowed: allow,
         ContactDate: new Date()
     };
 
-    console.log(data);
-    const sendEmail = firebase.functions().httpsCallable('sendContactEmail');
-    sendEmail({ data });
+    db.collection('contacts')
+        .add(data)
+        .then(docRef => {
+            console.log('Written doc: ', docRef.id);
+        })
+        .catch(error => {
+            console.log('Error writing document: ' + error);
+        }).then(() => {
+            var toastHTML = '<span>Message Sent!</span>';
+            M.toast({html: toastHTML});
+        });
 });
